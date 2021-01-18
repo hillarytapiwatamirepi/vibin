12adc10c691d3f6b46d0f5449471238c7fe34e4f
hillarytapiwatamirepi <46495638+hillarytapiwatamirepi@users.noreply.github.com>
2021-01-17 20:28:36 -0500

    added genius  api's

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express =  require("express");
 const app = express();
+const routes = require("./src/routes/api");
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
+
 
 
 // to automatically serve all the files in public, especially the html's
@@ -10,6 +12,13 @@ app.use(express.static("public"))
 // responsible for parsing body requests that happen through middleware
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
+
+
+// let's talk api's
+app.use("/api",routes);
+
+
+
 
 
 // let's listen 
---
+++ src/routes/api.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const express = require("express");
+const { getLyrics, getSong ,searchSong} = require('genius-lyrics-api');
+const getSongById = require("genius-lyrics-api/lib/getSongById");
+var router =express.Router();
+
+router.get("/getLyrics/:title/:artist",(req,res)=>{
+    // console.log(req)
+    var title = req.params.title;
+    var artist = req.params.artist;
+    const options = {
+        apiKey: process.env.CLIENT_ACCESS_TOKEN,
+        title: title,
+        artist: artist,
+        optimizeQuery: true
+        
+    };
+    getLyrics(options).then((lyrics)=>{
+            res.send(lyrics);
+    }).catch((err)=>{
+        console.log("this is my err",err)
+         res.send(err)
+    });
+
+})
+
+router.get("/getSong/:title/:artist",(req,res)=>{
+    // console.log(req)
+    var title = req.params.title;
+    var artist = req.params.artist;
+
+    const options = {
+        apiKey: process.env.CLIENT_ACCESS_TOKEN,
+        title: title,
+        artist: artist,
+        optimizeQuery: true
+        
+    };
+    getSong(options).then((data)=>{
+            res.send(data);
+    }).catch((err)=>{
+ 
+        res.send(err)
+    });
+
+})
+
+router.get("/searchSong/:title/:artist",(req,res)=>{
+    // console.log(req)
+    var title = req.params.title;
+    var artist = req.params.artist;
+    const options = {
+        apiKey: process.env.CLIENT_ACCESS_TOKEN,
+        title: title,
+        artist: artist,
+        optimizeQuery: true
+        
+    };
+    searchSong(options).then((data)=>{
+            res.send(data);
+    }).catch((err)=>{
+ 
+        res.send(err)
+    });
+
+})
+
+router.get("/getSongById/:id",(req,res)=>{
+    var id = req.params.id;
+    getSongById(id,process.env.CLIENT_ACCESS_TOKEN).then((data)=>{
+        res.send(data);
+    }).catch((err)=>{
+        res.send(err)
+    });
+
+})
+
+
+
+module.exports = router
